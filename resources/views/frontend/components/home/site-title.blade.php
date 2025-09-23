a2fa6af8eb170e5e9b845bd71b1c1f106f4ac4e1
Hemant kachhi <139211574+hemantkachhi2002@users.noreply.github.com>
2025-09-23 16:03:38 +0530

    Initial Laravel project setup

---
+++ resources/views/frontend/components/home/site-title.blade.php
@@ -0,0 +1,11 @@
+<!-- Begin Site Title
+================================================== -->
+<div class="container">
+	<div class="mainheading">
+		<h1 class="sitetitle">Indori Artist</h1>
+		<p class="lead">
+			All Artist are fantastic choice for all. Join Now.
+		</p>
+	</div>
+<!-- End Site Title
+================================================== -->
