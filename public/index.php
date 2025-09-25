f23d590d4c69eef034e45092113c8646cba9a594
Hemant kachhi <139211574+hemantkachhi2002@users.noreply.github.com>
2025-09-25 13:32:38 +0530

    Add vercel.json for deployment configuration

--- public/index.php
+++ public/index.php
@@ -10,13 +10,13 @@ define('LARAVEL_START', microtime(true));
 | Check If The Application Is Under Maintenance
 |--------------------------------------------------------------------------
 |
-| If the application is in maintenance / demo mode via the "down" command
+| If the application is in maintenance / demo mode via the "down" command,
 | we will load this file so that any pre-rendered content can be shown
 | instead of starting the framework, which could cause an exception.
 |
 */
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
@@ -25,31 +25,31 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 | Register The Auto Loader
 |--------------------------------------------------------------------------
 |
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
+| Composer provides an automatically generated class loader so we don’t
+| have to manually load classes. We just require it here.
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
 |
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
+| We create the application, run it via the HTTP kernel, and send
+| the response back to the client. We then terminate the request.
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
     $request = Request::capture()
-)->send();
+);
+
+$response->send();
 
 $kernel->terminate($request, $response);
